c5d219c1fe096623a87cc779cc5ab7591dc173db
jhon-murillo <104637388+jhon-murillo@users.noreply.github.com>
2022-09-30 14:31:42 +0200

    Create main.go
    http.Client

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http"
+)
+
+func listing() error {
+	var client *http.Client
+	var err error
+	if tracing {
+		client, err = createClientWithTracing()
+	} else {
+		client, err = createDefaultClient()
+	}
+	if err != nil {
+		return err
+	}
+
+	_ = client
+	return nil
+}
+
+var tracing bool
+
+func createClientWithTracing() (*http.Client, error) {
+	return nil, nil
+}
+
+func createDefaultClient() (*http.Client, error) {
+	return nil, nil
+}
