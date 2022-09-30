59f15ee65ee98a049d212b0e53d98f8f01ba09d5
jhon-murillo <104637388+jhon-murillo@users.noreply.github.com>
2022-09-30 16:39:16 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"net/http"
+	"time"
 	"bufio"
 	"io"
 	"os"
@@ -49,3 +51,49 @@ func createClientWithTracing() (*http.Client, error) {
 func createDefaultClient() (*http.Client, error) {
 	return nil, nil
 }
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	response, err := doSomeTask(r.Context(), r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	go func() {
+		err := publish(detach{ctx: r.Context()}, response)
+		// Do something with err
+		_ = err
+	}()
+
+	writeResponse(response)
+}
+
+type detach struct {
+	ctx context.Context
+}
+
+func (d detach) Deadline() (time.Time, bool) {
+	return time.Time{}, false
+}
+
+func (d detach) Done() <-chan struct{} {
+	return nil
+}
+
+func (d detach) Err() error {
+	return nil
+}
+
+func (d detach) Value(key any) any {
+	return d.ctx.Value(key)
+}
+
+func doSomeTask(context.Context, *http.Request) (string, error) {
+	return "", nil
+}
+
+func publish(context.Context, string) error {
+	return nil
+}
+
+func writeResponse(string) {}
