68800d9f5ea1769a02077dda0c886c50ad52b312
jhon-murillo <104637388+jhon-murillo@users.noreply.github.com>
2022-09-30 15:49:29 +0200

    Update main.go

--- main.go
+++ main.go
@@ -2,9 +2,29 @@ package main
 
 import (
 	"net/http"
+	"bufio"
+	"io"
+	"os"
 )
 
-func main() error {
+func main() {
+	listing()
+	file, err := os.Open("main.go")
+	if err != nil {
+		panic(err)
+	}
+	_, _ = countEmptyLines(file)
+}
+
+func countEmptyLines(reader io.Reader) (int, error) {
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		// ...
+	}
+	return 0, nil
+}
+
+func listing() error {
 	var client *http.Client
 	var err error
 	if tracing {
