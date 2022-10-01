c5ec4b7d1d4a5cc97cd021f3cc098a6cc71f5440
jhon-murillo <104637388+jhon-murillo@users.noreply.github.com>
2022-10-01 06:10:43 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,99 +1,12 @@
 package main
 
 import (
-	"context"
-	"net/http"
-	"time"
-	"bufio"
-	"io"
-	"os"
+    "github.com/hashicorp/go-retryablehttp"
 )
 
 func main() {
-	listing()
-	file, err := os.Open("main.go")
-	if err != nil {
-		panic(err)
-	}
-	_, _ = countEmptyLines(file)
+    resp, err := retryablehttp.Get("www.google.com")
+    if err != nil {
+    panic(err)
+    }
 }
-
-func countEmptyLines(reader io.Reader) (int, error) {
-	scanner := bufio.NewScanner(reader)
-	for scanner.Scan() {
-		// ...
-	}
-	return 0, nil
-}
-
-func listing() error {
-	var client *http.Client
-	var err error
-	if tracing {
-		client, err = createClientWithTracing()
-	} else {
-		client, err = createDefaultClient()
-	}
-	if err != nil {
-		return err
-	}
-
-	_ = client
-	return nil
-}
-
-var tracing bool
-
-func createClientWithTracing() (*http.Client, error) {
-	return nil, nil
-}
-
-func createDefaultClient() (*http.Client, error) {
-	return nil, nil
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	response, err := doSomeTask(r.Context(), r)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	go func() {
-		err := publish(detach{ctx: r.Context()}, response)
-		// Do something with err
-		_ = err
-	}()
-
-	writeResponse(response)
-}
-
-type detach struct {
-	ctx context.Context
-}
-
-func (d detach) Deadline() (time.Time, bool) {
-	return time.Time{}, false
-}
-
-func (d detach) Done() <-chan struct{} {
-	return nil
-}
-
-func (d detach) Err() error {
-	return nil
-}
-
-func (d detach) Value(key any) any {
-	return d.ctx.Value(key)
-}
-
-func doSomeTask(context.Context, *http.Request) (string, error) {
-	return "", nil
-}
-
-func publish(context.Context, string) error {
-	return nil
-}
-
-func writeResponse(string) {}
