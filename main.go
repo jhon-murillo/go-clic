76c7acd214b313bc634b1e86182d98d1e825cbe4
jhon-murillo <104637388+jhon-murillo@users.noreply.github.com>
2022-10-02 07:24:22 +0200

    Update main.go

--- main.go
+++ main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"log"
-
+        "client"
 	"github.com/hashicorp/go-retryablehttp"
 )
 
 func main() {
+	
+	resp, err := client.Get("https://golang.org/")
+		if err != nil {
+		panic(err)
+	}
+	log.Println(resp)
+	
 	resp, err := retryablehttp.Get("https://google.com")
 	if err != nil {
 		panic(err)
