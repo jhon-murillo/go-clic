060dff7cc3ed972c262125f84e4022ba32abe89a
jhon-murillo <104637388+jhon-murillo@users.noreply.github.com>
2022-10-03 06:06:23 +0200

    Update main.go

--- main.go
+++ main.go
@@ -3,9 +3,23 @@ package main
 import (
 	"log"
 	"github.com/hashicorp/go-retryablehttp"
+	"net"
+	"net/http"
+	"time"
 )
 
 func main() {
+	
+	client := &http.Client{
+		Timeout: 5 * time.Second,
+		Transport: &http.Transport{
+			DialContext: (&net.Dialer{
+				Timeout: time.Second,
+			}).DialContext,
+			TLSHandshakeTimeout:   time.Second,
+			ResponseHeaderTimeout: time.Second,
+		},
+	}
 	
 	resp, err := client.Get("https://golang.org/")
 		if err != nil {
