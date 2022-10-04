aa22fdf3cf34081b99fbc89cdeb24ce7d4fa45a0
jhon-murillo <104637388+jhon-murillo@users.noreply.github.com>
2022-10-04 16:48:15 +0200

    Update main.go

--- main.go
+++ main.go
@@ -21,28 +21,8 @@ func main() {
 		u, err := url.ParseRequestURI(os.Args[i])
 	        if err != nil {
 	        panic(err)
+		}
 	}
-        
-        
-    }
-	
-	for i, element := range someSlice {
-   
-}
-	
-	u, err := url.ParseRequestURI(os.Args[1])
-	if err != nil {
-	    panic(err)
-	}
-	
-	f, err := os.OpenFile("url.list", os.O_RDWR|os.O_CREATE, 0755)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if err := f.Close(); err != nil {
-		log.Fatal(err)
-	}
-}
 	
 	client := &http.Client{
 		Timeout: 5 * time.Second,
@@ -71,56 +51,4 @@ func main() {
 	log.Printf("Body Size:", string(body))
 	
 	defer resp.Body.Close()
-}
-
-func UrlToLines(url string) ([]string, error) {
-	client := &http.Client{
-		Timeout: 5 * time.Second,
-		Transport: &http.Transport{
-			DialContext: (&net.Dialer{
-				Timeout: time.Second,
-			}).DialContext,
-			TLSHandshakeTimeout:   time.Second,
-			ResponseHeaderTimeout: time.Second,
-		},
-	}
-	
-	resp, err := client.Get(u.String())
-	if err != nil {
-	    panic(err)
-	}
-	resp, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-	return LinesFromReader(resp.Body)
-}
-
-func LinesFromReader(r io.Reader) ([]string, error) {
-	var lines []string
-	scanner := bufio.NewScanner(r)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	if err := scanner.Err(); err != nil {
-		return nil, err
-	}
-
-	return lines, nil
-}
-
-func newWatcher() watcher {
-	w := watcher{}
-	go w.watch()
-	return w
-}
-
-type watcher struct { /* Some resources */
-}
-
-func (w watcher) watch() {}
-
-func (w watcher) close() {
-	// Close the resources
 }
