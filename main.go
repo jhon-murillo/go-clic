2b7cf5644da10f346147c13d147c9c55cd83d8f4
jhon-murillo <104637388+jhon-murillo@users.noreply.github.com>
2022-10-04 13:10:34 +0200

    Update main.go

--- main.go
+++ main.go
@@ -9,19 +9,35 @@ import (
 	"net/http"
 	"net/url"
 	"time"
-	"io/ioutil"
-
+	"bufio"
 )
 
 func main() {
+	var f *File
 	if len(os.Args) != 2 {
+	    f, err = os.OpenFile("url.list", os.O_RDWR|os.O_CREATE, 0755)
+		
 	    log.Printf("Usage: %s URL\n", filepath.Base(os.Args[0]))
+		
+            if err = f.Close(); err != nil {
+		log.Fatal(err)
+	    }	
+	    
 	}
 	
 	u, err := url.ParseRequestURI(os.Args[1])
 	if err != nil {
 	    panic(err)
 	}
+	
+	f, err := os.OpenFile("url.list", os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := f.Close(); err != nil {
+		log.Fatal(err)
+	}
+}
 	
 	client := &http.Client{
 		Timeout: 5 * time.Second,
@@ -50,4 +66,56 @@ func main() {
 	log.Printf("Body Size:", string(body))
 	
 	defer resp.Body.Close()
+}
+
+func UrlToLines(url string) ([]string, error) {
+	client := &http.Client{
+		Timeout: 5 * time.Second,
+		Transport: &http.Transport{
+			DialContext: (&net.Dialer{
+				Timeout: time.Second,
+			}).DialContext,
+			TLSHandshakeTimeout:   time.Second,
+			ResponseHeaderTimeout: time.Second,
+		},
+	}
+	
+	resp, err := client.Get(u.String())
+	if err != nil {
+	    panic(err)
+	}
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	return LinesFromReader(resp.Body)
+}
+
+func LinesFromReader(r io.Reader) ([]string, error) {
+	var lines []string
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return lines, nil
+}
+
+func newWatcher() watcher {
+	w := watcher{}
+	go w.watch()
+	return w
+}
+
+type watcher struct { /* Some resources */
+}
+
+func (w watcher) watch() {}
+
+func (w watcher) close() {
+	// Close the resources
 }
